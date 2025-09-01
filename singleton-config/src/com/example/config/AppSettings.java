57798c5e6427157162e593be08d5b59afe40108a
AttAditya <attachment.aditya@gmail.com>
2025-09-01 14:19:34 +0530

    Corrected for Reflection and Thread Safety

--- singleton-config/src/com/example/config/AppSettings.java
+++ singleton-config/src/com/example/config/AppSettings.java
@@ -5,16 +5,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Properties;
 
+import javax.management.InstanceAlreadyExistsException;
+
 public class AppSettings implements Serializable {
     private final Properties props = new Properties();
-    
-    private AppSettings() {}
     private static AppSettings instance;
+    
+    private AppSettings() throws InstanceAlreadyExistsException {
+        if (instance != null)
+            throw new InstanceAlreadyExistsException(
+                "AppSettings Instance already exists"
+            );
 
-    public static AppSettings getInstance() {
-        if (instance == null)
-            instance = new AppSettings();
-        return instance;
+        instance = this;
+    }
+
+    public static synchronized AppSettings getInstance() {
+        try {
+            return new AppSettings();
+        } catch (InstanceAlreadyExistsException ignored) {
+            return instance;
+        }
     }
 
     public void loadFromFile(Path file) {
