666069bea59432fcf3f70c26c9065270a3dbd304
AttAditya <attachment.aditya@gmail.com>
2025-09-08 17:45:35 +0530

    Decorator Coffee

---
+++ beverages_decorator/ChocolateDecorator.java
@@ -0,0 +1,14 @@
+package beverages_decorator;
+
+public class ChocolateDecorator extends Beverage {
+    private final Beverage beverage;
+
+    public ChocolateDecorator(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public int cost() {
+        return this.beverage.cost() + 10;
+    }
+}
--- beverages_decorator/Client.java
+++ beverages_decorator/Client.java
@@ -1,17 +1,18 @@
 package beverages_decorator;
 
 public class Client {
-
 	public static void main(String[] args) {
-		
-
-		
 		Beverage coffee = new Cappuccino();
         System.out.println("Coffee");
 		System.out.println(coffee.cost());
-		
-		
-
+		coffee = new ChocolateDecorator(coffee);
+		System.out.println(coffee.cost());
+		coffee = new ChocolateDecorator(coffee);
+		System.out.println(coffee.cost());
+		coffee = new ChocolateDecorator(coffee);
+		System.out.println(coffee.cost());
+		coffee = new ChocolateDecorator(coffee);
+		System.out.println(coffee.cost());
 	}
 
 }
